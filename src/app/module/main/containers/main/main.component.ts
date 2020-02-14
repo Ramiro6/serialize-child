028b501d5c46aaffea36efa602bd84046c241268
ramiro6 <ramiroruiz578@gmail.com>
2020-02-14 16:30:32 +0100

    add basic search

--- src/app/module/main/containers/main/main.component.ts
+++ src/app/module/main/containers/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { GetDataService } from '../../services/get-data/get-data.service';
 
 @Component({
@@ -7,10 +7,64 @@ import { GetDataService } from '../../services/get-data/get-data.service';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  @ViewChild('input') inputElement: ElementRef;
+  @ViewChildren('liSelect', { read: ElementRef }) liSelect: QueryList<ElementRef>;
+
+
+  itemOrder = ['file 1', 'file 2', 'file 3', 'file 4', 'file 5', 'file 6'];
 
   constructor( private api: GetDataService ) { }
 
   ngOnInit(): void {
+
   }
 
+
+  onClick(value): void {
+    const natiInput: HTMLInputElement = this.inputElement.nativeElement;
+    natiInput.value = value;
+  }
+
+  searchBar(term: KeyboardEvent): void {
+    if (term.keyCode === 40) {
+      this.liSelect.first.nativeElement.focus();
+      // const listItem: HTMLInputElement = this.liSelect.nativeElement;
+      // listItem.focus();
+      // console.log(this.liSelect.nativeElement);
+    } else if (term.keyCode === 38) {
+      this.liSelect.last.nativeElement.focus();
+    }
+    // console.log(term);
+  }
+
+  changeKeyDown(item, id): void {
+    const getElement: ElementRef = this.liSelect.find((itemNoused: ElementRef, idName: number) => {
+      return idName === id + 1;
+    });
+
+
+    if (getElement) {
+      const getNative: HTMLElement = getElement.nativeElement;
+      getNative.focus();
+    } else {
+      this.liSelect.last.nativeElement.blur();
+      this.inputElement.nativeElement.focus();
+    }
+  }
+
+
+  changeKeyUp(item, id): void {
+    const getElement: ElementRef = this.liSelect.find((itemNoused: ElementRef, idName: number) => {
+      return idName === id - 1;
+    });
+
+
+    if (getElement) {
+      const getNative: HTMLElement = getElement.nativeElement;
+      getNative.focus();
+    } else {
+      this.liSelect.first.nativeElement.blur();
+      this.inputElement.nativeElement.focus();
+    }
+  }
 }
--- src/app/module/main/main.module.ts
+++ src/app/module/main/main.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './containers/main/main.component';
 import { GetDataService } from './services/get-data/get-data.service';
+import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     MainComponent
@@ -9,7 +10,10 @@ import { GetDataService } from './services/get-data/get-data.service';
   exports: [
     MainComponent
   ],
-  imports: [ CommonModule ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   providers: [
     GetDataService
   ]
